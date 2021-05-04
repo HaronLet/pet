d8e40ed085e8334375675dce4ff489c8491e636d
Агапов Михаил <muhaxt@yandex.ru>
2021-05-04 12:29:21 +0300

    Работа на кнопкой

---
+++ src/components/Button.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name = "";
+  export let img = "";
+  export let alt = "";
+</script>
+
+<button>
+  {#if img !== ""}
+    <img src="{img}" alt="{alt}">
+  {/if}  
+
+  {#if name !== ""}
+    {name}
+  {/if}  
+</button>
+
+<style>
+  button {
+    display: flex;
+    align-items: center;
+    padding: 5px;
+    margin: 5px;
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+  }
+</style>
---
+++ src/components/MainMenu.svelte
@@ -0,0 +1,33 @@
+<script>
+  import App from '../App.svelte';
+import Button from './Button.svelte';
+import Fieldset from './Fieldset.svelte';
+
+  const papka = {
+    //name: "Папка",
+    img: "https://img.icons8.com/metro/344/add-folder.png",
+    alt: "Иконка папки"
+  };
+  const lsr = {
+    name: "Локальная смета",
+    img: "https://img.icons8.com/metro/344/add-folder.png",
+    alt: "Иконка папки"
+  };
+  const ps = {
+    name: "Проектная смета",
+    img: "https://img.icons8.com/metro/344/add-folder.png",
+    alt: "Иконка папки"
+  };
+  const ssr = {
+    name: "Сводный сметный расчёт",
+    img: "https://img.icons8.com/metro/344/add-folder.png",
+    alt: "Иконка папки"
+  };
+</script>
+
+<Button {...papka}/>
+<Button {...lsr}/>
+<Button {...ps}/>
+<Button {...ssr}/>
+<Button name={"+"}/>
+
